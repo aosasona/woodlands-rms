a228d5ecd5866792d6e531d35505e9d2794ae4ca
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-04-28 01:16:35 +0100

    More db-related setup

--- data/migrations/00001_create_users_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-  id INTEGER PRIMARY KEY,
-  email TEXT UNIQUE NOT NULL,
-  password TEXT, -- in case of OAuth, this field will be NULL
-  created_at INTEGER NOT NULL DEFAULT (unixepoch()),
-  last_updated INTEGER NOT NULL DEFAULT (unixepoch())
-);
