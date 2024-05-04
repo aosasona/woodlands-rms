83e367a1764b21d73dfc788b4a6e5f75bc470334
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-05-04 16:04:14 +0100

    Added franken-ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,34 @@
+import preset from "franken-ui/shadcn-ui/preset";
+import variables from "franken-ui/shadcn-ui/variables";
+import ui from "franken-ui";
+import hooks from "franken-ui/shadcn-ui/hooks";
+
+const shadcn = hooks()
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./pages/**/*.{html,php,js}", "./src/**/*.{html,php,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  presets: [preset],
+  plugins: [variables({ theme: "#81057F" }), ui({
+    components: {
+      button: {
+        hooks: {}
+      },
+      'form-range': {
+        hooks: {}
+      },
+      form: {
+        hooks: {},
+        media: true
+      },
+      notification: {
+        hooks: {},
+        media: true
+      }
+    }
+  })],
 }
 
